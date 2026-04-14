432b0577c5abd5be450828a08963d911d9110453
Iven Winkelmann <winkelmann.iven@gmail.com>
2026-04-14 14:51:24 +0200

    Flatten rig submodule

--- gantry-core/src/tools/mod.rs
+++ gantry-core/src/tools/mod.rs
@@ -1,1 +1,14 @@
-pub mod rig;
+mod boundary;
+mod messages;
+
+pub mod edit;
+pub mod grep;
+pub mod read;
+pub mod tree;
+pub mod write;
+
+pub use edit::EditTool;
+pub use grep::GrepTool;
+pub use read::ReadTool;
+pub use tree::TreeTool;
+pub use write::WriteTool;
